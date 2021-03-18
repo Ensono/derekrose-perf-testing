63a6ef84ec086fb306f830a764bf5602230fb155
Sam Greig <samgreig@AML0280.local>
2021-03-18 16:48:38 +0000

    initial commit

---
+++ src/env-config.js
@@ -0,0 +1,20 @@
+const user = __ENV.USER
+const password = __ENV.PASSWORD
+const environment = __ENV.ENVIRONMENT
+const credentials = `${user}:${password}`
+
+if(environment == 'DEV') {
+    baseUrl = `https://${credentials}@dev.derekrose.com`
+}
+
+if(environment == 'TEST') {
+    baseUrl = `https://${credentials}@test.derekrose.com`
+}
+
+if(environment == 'PROD') {
+    baseUrl = `https://${credentials}@prod.derekrose.com`
+}
+
+module.exports = {
+    baseUrl,
+};
